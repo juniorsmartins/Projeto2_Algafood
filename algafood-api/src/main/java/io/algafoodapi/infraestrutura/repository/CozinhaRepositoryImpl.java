c29579edc0fa1b1b8082c56ab66d5098d147c0b8
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-11-28 18:57:56 -0400

    Addedd estrutura de repositório.

---
+++ algafood-api/src/main/java/io/algafoodapi/infraestrutura/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,30 @@
+package io.algafoodapi.infraestrutura.repository;
+
+import io.algafoodapi.domain.model.Cozinha;
+import io.algafoodapi.domain.repository.CozinhaRepository;
+
+import java.util.List;
+
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+
+    @Override
+    public List<Cozinha> listar() {
+        return null;
+    }
+
+    @Override
+    public Cozinha buscar(Long id) {
+        return null;
+    }
+
+    @Override
+    public Cozinha salvar(Cozinha cozinha) {
+        return null;
+    }
+
+    @Override
+    public void remover(Cozinha cozinha) {
+
+    }
+}
