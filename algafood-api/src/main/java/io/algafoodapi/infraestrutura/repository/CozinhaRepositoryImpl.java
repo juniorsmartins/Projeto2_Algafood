6791a494cbc0d3b2b250fd8e2bbe1b2307d5294a
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-11-28 19:04:51 -0400

    Addedd estrutura de repositório.

--- algafood-api/src/main/java/io/algafoodapi/infraestrutura/repository/CozinhaRepositoryImpl.java
+++ algafood-api/src/main/java/io/algafoodapi/infraestrutura/repository/CozinhaRepositoryImpl.java
@@ -2,29 +2,39 @@ package io.algafoodapi.infraestrutura.repository;
 
 import io.algafoodapi.domain.model.Cozinha;
 import io.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Component
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
+    @PersistenceContext
+    private EntityManager manager;
 
     @Override
     public List<Cozinha> listar() {
-        return null;
+        return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
     }
 
     @Override
     public Cozinha buscar(Long id) {
-        return null;
+        return manager.find(Cozinha.class, id);
     }
 
+    @Transactional
     @Override
     public Cozinha salvar(Cozinha cozinha) {
-        return null;
+        return manager.merge(cozinha);
     }
 
+    @Transactional
     @Override
     public void remover(Cozinha cozinha) {
-
+        cozinha = buscar(cozinha.getId());
+        manager.remove(cozinha);
     }
 }
