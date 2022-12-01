2835210014962bdebed733f325e5b038e3a92c58
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-01 14:11:29 -0400

    Added entidade Cidade.

---
+++ algafood-api/src/main/java/io/algafoodapi/domain/model/Cidade.java
@@ -0,0 +1,30 @@
+package io.algafoodapi.domain.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "cidades")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public final class Cidade implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome", length = 80, nullable = false)
+    private String nome;
+}
+
