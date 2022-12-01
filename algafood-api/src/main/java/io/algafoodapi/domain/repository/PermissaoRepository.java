980d54a5d6862dac26af90048cf9e8bde444b954
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-01 15:03:04 -0400

    Added pattern repository of Permissao.

---
+++ algafood-api/src/main/java/io/algafoodapi/domain/repository/PermissaoRepository.java
@@ -0,0 +1,13 @@
+package io.algafoodapi.domain.repository;
+
+import io.algafoodapi.domain.model.Permissao;
+
+import java.util.List;
+
+public interface PermissaoRepository {
+
+    List<Permissao> listar();
+    Permissao buscar(Long id);
+    Permissao salvar(Permissao permissao);
+    void remover(Permissao permissao);
+}
