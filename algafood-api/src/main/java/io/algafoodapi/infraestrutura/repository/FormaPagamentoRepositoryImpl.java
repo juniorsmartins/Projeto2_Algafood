24a37689c3221113f559681de7b6f0ad385030ab
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-01 14:58:32 -0400

    Added pattern repository of FormaPagamento.

---
+++ algafood-api/src/main/java/io/algafoodapi/infraestrutura/repository/FormaPagamentoRepositoryImpl.java
@@ -0,0 +1,40 @@
+package io.algafoodapi.infraestrutura.repository;
+
+import io.algafoodapi.domain.model.FormaPagamento;
+import io.algafoodapi.domain.repository.FormaPagamentoRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class FormaPagamentoRepositoryImpl implements FormaPagamentoRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<FormaPagamento> listar() {
+        return this.manager.createQuery("from FormaPagamento", FormaPagamento.class).getResultList();
+    }
+
+    @Override
+    public FormaPagamento buscar(Long id) {
+        return this.manager.find(FormaPagamento.class, id);
+    }
+
+    @Transactional
+    @Override
+    public FormaPagamento salvar(FormaPagamento formaPagamento) {
+        return this.manager.merge(formaPagamento);
+    }
+
+    @Transactional
+    @Override
+    public void remover(FormaPagamento formaPagamento) {
+        formaPagamento = buscar(formaPagamento.getId());
+        this.manager.remove(formaPagamento);
+    }
+}
