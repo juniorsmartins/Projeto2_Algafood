011e85a945211c1c46f52d4c9e21f0adf05a8040
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-03 18:25:52 -0400

    ...

--- algafood-api/src/main/java/io/algafoodapi/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/io/algafoodapi/api/controller/CozinhaController.java
@@ -3,14 +3,16 @@ package io.algafoodapi.api.controller;
 import io.algafoodapi.domain.model.Cozinha;
 import io.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/cozinhas")
+@RequestMapping(value = "/cozinhas", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class CozinhaController {
 
     @Autowired
@@ -19,5 +21,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return this.cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable(name = "id") Long id) {
+        return this.cozinhaRepository.buscar(id);
     }
 }
