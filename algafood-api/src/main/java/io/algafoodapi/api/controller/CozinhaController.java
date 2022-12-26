02cb8f731afb0c98b93e06d468e086017f50c1a7
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-26 11:25:50 -0400

    Added more features in Cozinha.

--- algafood-api/src/main/java/io/algafoodapi/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/io/algafoodapi/api/controller/CozinhaController.java
@@ -3,14 +3,12 @@ package io.algafoodapi.api.controller;
 import io.algafoodapi.domain.model.Cozinha;
 import io.algafoodapi.domain.model.CozinhasXmlWrapper;
 import io.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +18,46 @@ public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void adicionar(@RequestBody Cozinha cozinha) {
+        this.cozinhaRepository.salvar(cozinha);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable(name = "id") Long id, @RequestBody Cozinha cozinhaAtual) {
+
+        var cozinha = this.cozinhaRepository.buscar(id);
+
+        if(cozinha == null)
+            return ResponseEntity
+                    .notFound()
+                    .build();
+
+        BeanUtils.copyProperties(cozinhaAtual, cozinha, "id");
+        this.cozinhaRepository.salvar(cozinha);
+
+        return ResponseEntity
+                .ok(cozinha);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Cozinha> remover(@PathVariable(name = "id") Long id) {
+
+        var cozinha = this.cozinhaRepository.buscar(id);
+
+        if(cozinha == null)
+            return ResponseEntity
+                    .notFound()
+                    .build();
+
+        this.cozinhaRepository.remover(cozinha);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
@@ -37,7 +75,9 @@ public class CozinhaController {
         var cozinha = this.cozinhaRepository.buscar(id);
 
         if(cozinha == null)
-            return ResponseEntity.notFound().build();
+            return ResponseEntity
+                    .notFound()
+                    .build();
 
         return ResponseEntity
                 .ok(cozinha);
