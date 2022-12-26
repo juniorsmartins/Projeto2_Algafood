04e22063f081991f74f232d9857ee464e40559e0
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-26 14:01:01 -0400

    Retirada de XML

--- algafood-api/src/main/java/io/algafoodapi/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/io/algafoodapi/api/controller/CozinhaController.java
@@ -3,21 +3,18 @@ package io.algafoodapi.api.controller;
 import io.algafoodapi.domain.exception.EntidadeEmUsoException;
 import io.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import io.algafoodapi.domain.model.Cozinha;
-import io.algafoodapi.domain.model.CozinhasXmlWrapper;
 import io.algafoodapi.domain.repository.CozinhaRepository;
 import io.algafoodapi.domain.service.CadastroCozinhaService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/cozinhas", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }) // Content Negotiation (Json e XML)
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 
     @Autowired
@@ -71,14 +68,9 @@ public class CozinhaController {
                 .build();
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public List<Cozinha> listar() {
         return this.cozinhaRepository.listar();
-    }
-
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public CozinhasXmlWrapper listarXml() {
-        return new CozinhasXmlWrapper(this.cozinhaRepository.listar());
     }
 
     @GetMapping(value = "/{id}")
