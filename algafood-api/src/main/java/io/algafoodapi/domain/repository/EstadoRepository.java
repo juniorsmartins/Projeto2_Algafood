6c6d3237ca493b6d2d8bf8c5c3c13e94b60b8c77
Junior Martins <juniorsoaresmartins@gmail.com>
2023-01-05 15:49:11 -0400

    ...

--- algafood-api/src/main/java/io/algafoodapi/domain/repository/EstadoRepository.java
+++ algafood-api/src/main/java/io/algafoodapi/domain/repository/EstadoRepository.java
@@ -2,5 +2,8 @@ package io.algafoodapi.domain.repository;
 
 import io.algafoodapi.domain.model.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long> { }
+
