280e5475eccad783c3753f186ae8e90c11d1b102
Junior Martins <juniorsoaresmartins@gmail.com>
2023-01-07 10:08:47 -0400

    Add Spring Data JPA com padrão de Specification DDD com SDJ.

--- algafood-api/src/main/java/io/algafoodapi/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/io/algafoodapi/api/controller/RestauranteController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Map;
 
