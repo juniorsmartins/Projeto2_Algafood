8b62b7ff00e41670c9d75819f6cdfaca9b1e8ece
Junior Martins <juniorsoaresmartins@gmail.com>
2023-01-23 19:03:15 -0400

    Inclusão de afterMigrate.sql com On Conflict

--- algafood-api/src/main/resources/db/migration/afterMigrate.sql
+++ algafood-api/src/main/resources/db/migration/afterMigrate.sql
@@ -1,8 +1,26 @@
+INSERT INTO estados(id, nome) VALUES
+    (1, 'Rio Grande do Sul'),
+    (2, 'Santa Catarina'),
+    (3, 'Paraná'),
+    (4, 'São Paulo'),
+    (5, 'Mato Grosso'),
+    (6, 'Minas Gerais')
+    On CONFLICT(id) DO NOTHING;
 
+INSERT INTO cidades(id, nome, estado_id) VALUES
+    (1, 'Porto Alegre', 1),
+    (2, 'Dom Pedrito', 1),
+    (3, 'São José', 2),
+    (4, 'Toledo', 3),
+    (5, 'Cuiabá', 5),
+    (6, 'Iturama', 6)
+    On CONFLICT(id) DO NOTHING;
 
-INSERT INTO cozinhas(id, nome) VALUES(1, 'Brasileira');
-INSERT INTO cozinhas(id, nome) VALUES(2, 'Japonesa');
-INSERT INTO cozinhas(id, nome) VALUES(3, 'Italiana');
-INSERT INTO cozinhas(id, nome) VALUES(4, 'Estadunidense');
+INSERT INTO cozinhas(id, nome) VALUES
+    (1, 'Brasileira'),
+    (2, 'Japonesa'),
+    (3, 'Italiana'),
+    (4, 'Estadunidense')
+    On CONFLICT(id) DO NOTHING;
 
 
