db7b60b23cb218384f343b567c6fdfbf968ca8d5
Junior Martins <juniorsoaresmartins@gmail.com>
2023-01-23 18:45:04 -0400

    ...

---
+++ algafood-api/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO cozinhas(id, nome) VALUES(1, 'Brasileira');
+INSERT INTO cozinhas(id, nome) VALUES(2, 'Japonesa');
+INSERT INTO cozinhas(id, nome) VALUES(3, 'Italiana');
+INSERT INTO cozinhas(id, nome) VALUES(4, 'Estadunidense');
+
+
