c2ba537f87fad9cd5faa13a0c245034f3e222cba
Junior Martins <juniorsoaresmartins@gmail.com>
2023-02-10 17:51:42 -0400

    Add tratamento para exceptions MediaType and RequisiçãoMalFormulada.

--- algafood-api/src/main/java/io/algafoodapi/domain/exception/ControleDeTratamentoDeExceptions.java
+++ algafood-api/src/main/java/io/algafoodapi/domain/exception/ControleDeTratamentoDeExceptions.java
@@ -1,9 +1,11 @@
 package io.algafoodapi.domain.exception;
 
+import io.algafoodapi.domain.exception.http400.RequisicaoMalFormuladaException;
 import io.algafoodapi.domain.exception.http404.EntidadeNaoEncontradaException;
 import io.algafoodapi.domain.exception.http409.EntidadeEmUsoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -39,6 +41,38 @@ public final class ControleDeTratamentoDeExceptions {
                 .dataHora(LocalDateTime.now())
                 .build();
 
+
+        return ResponseEntity
+                .status(httpStatus)
+                .body(mensagem);
+    }
+
+    @ExceptionHandler(RequisicaoMalFormuladaException.class)
+    public ResponseEntity<MensagemDeErro> tratarRequisicaoMalFormuladaException(RequisicaoMalFormuladaException malFormuladaException) {
+
+        var httpStatus = HttpStatus.BAD_REQUEST;
+
+        var mensagem = MensagemDeErro.builder()
+                .mensagem(malFormuladaException.getMessage())
+                .status(httpStatus)
+                .dataHora(LocalDateTime.now())
+                .build();
+
+        return ResponseEntity
+                .status(httpStatus)
+                .body(mensagem);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<MensagemDeErro> tratarHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException mediaTypeNotSupportedException) {
+
+        var httpStatus = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
+
+        var mensagem = MensagemDeErro.builder()
+                .mensagem("Tipo de mídia não aceito!")
+                .status(httpStatus)
+                .dataHora(LocalDateTime.now())
+                .build();
 
         return ResponseEntity
                 .status(httpStatus)
