799b9467f0dcd2d7e65dcf26c7877ec6c1ed24cd
Junior Martins <juniorsoaresmartins@gmail.com>
2023-02-14 13:24:54 -0400

    Criação de anotação personalizada de bean validation.

--- algafood-api/src/main/java/io/algafoodapi/domain/model/Restaurante.java
+++ algafood-api/src/main/java/io/algafoodapi/domain/model/Restaurante.java
@@ -2,32 +2,33 @@ package io.algafoodapi.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.algafoodapi.domain.core.validation.GruposValid;
+import io.algafoodapi.domain.core.validation.TaxaFrete;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Column;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Embedded;
-import javax.persistence.ManyToMany;
 import javax.persistence.JoinColumn;
-import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.JoinTable;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 import java.io.Serializable;
@@ -59,7 +60,8 @@ public final class Restaurante implements Serializable {
     @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 
-    @PositiveOrZero(message = "{TaxaFrete.invalida}")
+    @TaxaFrete
+//    @PositiveOrZero(message = "{TaxaFrete.invalida}")
     @Column(name = "taxa_frete", columnDefinition = "decimal(10.2) default 0.00", nullable = false)
     private BigDecimal taxaFrete = BigDecimal.ZERO;
 
