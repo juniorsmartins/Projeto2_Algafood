72c6d3c3e81b59d79813916b46aafbf4505fc8ff
Junior Martins <juniorsoaresmartins@gmail.com>
2023-02-17 06:27:50 -0400

    Testes de API com RestAssured

--- algafood-api/src/test/java/io/algafoodapi/CrudCozinhaAPITests.java
+++ algafood-api/src/test/java/io/algafoodapi/CrudCozinhaAPITests.java
@@ -2,6 +2,8 @@ package io.algafoodapi;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -12,17 +14,34 @@ class CrudCozinhaAPITests {
     @LocalServerPort
     private int porta;
 
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Habilita logs pra quando testes falharem
+        RestAssured.port = porta;
+    }
+
     @Test
     void deveRetornarStatus200_quandoConsultarCozinhas() {
         RestAssured
                 .given()
                     .basePath("/v1/cozinhas")
-                    .port(porta)
                     .accept(ContentType.JSON)
                 .when()
                     .get()
                 .then()
                     .statusCode(200);
+    }
+
+    @Test
+    void deveConterQuatroCozinhas_quandoListarCozinhas() {
+        RestAssured
+                .given()
+                    .basePath("/v1/cozinhas")
+                    .accept(ContentType.JSON)
+                .when()
+                    .get()
+                .then()
+                    .body("", Matchers.hasSize(4));
     }
 }
 
