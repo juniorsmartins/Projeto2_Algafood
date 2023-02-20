341b6d08741c6375bd5a9d671458016030fe9e2e
Junior Martins <juniorsoaresmartins@gmail.com>
2023-02-20 11:01:55 -0400

    Uso de DTOs em Cidade.

--- algafood-api/src/main/java/io/algafoodapi/domain/service/CidadeService.java
+++ algafood-api/src/main/java/io/algafoodapi/domain/service/CidadeService.java
@@ -3,10 +3,10 @@ package io.algafoodapi.domain.service;
 import io.algafoodapi.domain.core.Constantes;
 import io.algafoodapi.domain.exception.http400.RequisicaoMalFormuladaException;
 import io.algafoodapi.domain.exception.http404.CidadeNaoEncontradaException;
-import io.algafoodapi.domain.exception.http404.EstadoNaoEncontradoException;
 import io.algafoodapi.domain.exception.http409.CidadeEmUsoException;
 import io.algafoodapi.domain.model.Cidade;
 import io.algafoodapi.domain.repository.CidadeRepository;
+import io.algafoodapi.domain.repository.EstadoRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -17,48 +17,45 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class CidadeService {
 
     private final CidadeRepository cidadeRepository;
-    private final EstadoService estadoService;
+    private final EstadoRepository estadoRepository;
 
-    public CidadeService(final CidadeRepository cidadeRepository, final EstadoService estadoService) {
+    public CidadeService(final CidadeRepository cidadeRepository, final EstadoRepository estadoRepository) {
         this.cidadeRepository = cidadeRepository;
-        this.estadoService = estadoService;
+        this.estadoRepository = estadoRepository;
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
     public Cidade criar(Cidade cidade) {
 
-        var idEstado = cidade.getEstado().getId();
-        var estado = Optional.of(this.estadoService.consultarPorId(idEstado))
-            .orElseThrow(() ->
-                new RequisicaoMalFormuladaException(String.format(Constantes.ESTADO_NAO_ENCONTRADO, idEstado)));
-        cidade.setEstado(estado);
+        this.validarEstado(cidade);
 
         return this.cidadeRepository.saveAndFlush(cidade);
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
-    public Cidade atualizar(Long id, Cidade cidadeAtual) {
+    public Cidade atualizar(Long idCidade, Cidade cidade) {
 
-        var cidade = this.consultarPorId(id);
+        this.validarEstado(cidade);
 
-        try {
-            var estado = this.estadoService.consultarPorId(cidadeAtual.getEstado().getId());
-            cidadeAtual.setEstado(estado);
+        var cidadeParaAtualizar = this.consultarPorId(idCidade);
 
-        } catch (EstadoNaoEncontradoException naoEncontradaException) {
-            throw new RequisicaoMalFormuladaException(naoEncontradaException.getMessage(), naoEncontradaException);
-        }
+        BeanUtils.copyProperties(cidade, cidadeParaAtualizar, "id");
 
-        BeanUtils.copyProperties(cidadeAtual, cidade, "id");
+        return this.cidadeRepository.saveAndFlush(cidadeParaAtualizar);
+    }
 
-        return this.cidadeRepository.saveAndFlush(cidade);
+    private void validarEstado(Cidade cidade) {
+        var idEstado = cidade.getEstado().getId();
+        var estado = this.estadoRepository.findById(idEstado)
+                .orElseThrow(() ->
+                        new RequisicaoMalFormuladaException(String.format(Constantes.ESTADO_NAO_ENCONTRADO, idEstado)));
+        cidade.setEstado(estado);
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
